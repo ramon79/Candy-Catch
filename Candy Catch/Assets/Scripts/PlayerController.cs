b40f0b560f55a261cc92d4109be869667aa2ec54
ramon79 <47372895+ramon79@users.noreply.github.com>
2019-03-03 14:21:50 +0900

    finished

---
+++ Candy Catch/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public bool canMove = true;
+
+	//[SerializeField]	
+	//float maxPos;
+
+	[SerializeField]
+	float moveSpeed;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(canMove)
+		{
+			Move();
+
+		}
+		
+	}
+
+	private void Move()
+	{
+		float inputX = Input.GetAxis("Horizontal");
+
+		transform.position += Vector3.right * inputX * moveSpeed * Time.deltaTime;
+		
+		// float xPos = Mathf.Clamp(transform.position.x, -maxPos, maxPos);
+		float xPos = Mathf.Clamp(transform.position.x, -65f, -50f);
+
+		transform.position = new Vector3(xPos, transform.position.y, transform.position.z);
+
+	}
+}
